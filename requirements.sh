7db9b2b594f29cdb1f9368f17c4a9958cdde4620
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-05-31 01:39:33 +0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-pip install flask flask_cors sqlalchemy mysqlclient python-dateutil flask-wft
+pip install flask flask_cors sqlalchemy mysqlclient python-dateutil flask-wtf flasgger
 export TZ="Asia/Istanbul"
 export FLAYERFX_MYSQL_USER=flayerfx
 export FLAYERFX_MYSQL_HOST=flayerfx.mysql.pythonanywhere-services.com
