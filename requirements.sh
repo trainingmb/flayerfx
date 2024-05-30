fb716cdceb4f719baefed2a229b935e554eb02ae
trainingmb <118762148+trainingmb@users.noreply.github.com>
2024-05-31 01:14:44 +0300

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 pip install flask flask_cors sqlalchemy mysqlclient python-dateutil flask-wft
 export TZ="Asia/Istanbul"
-export INADIS_MYSQL_USER=stofgenius
-export INADIS_MYSQL_HOST=stofgenius.mysql.pythonanywhere-services.com
-export INADIS_MYSQL_DB="stofgenius$stories"
-export INADIS_ENV="dev"
-export INADIS_TYPE_STORAGE=db
+export FLAYERFX_MYSQL_USER=flayerfx
+export FLAYERFX_MYSQL_HOST=flayerfx.mysql.pythonanywhere-services.com
+export FLAYERFX_MYSQL_DB="flayerfx$scrap"
+export FLAYERFX_ENV="dev"
+export FLAYERFX_TYPE_STORAGE=db
